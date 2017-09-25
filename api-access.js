69838ebfde772b498e62871ffc316cba5b3a85e3
robertpan226 <robertpan226@gmail.com>
2017-09-25 00:43:43 -0400

    a

--- api-access.js
+++ api-access.js
@@ -3,10 +3,23 @@ var uwclient = new uwaterlooApi({
 	API_KEY : "2c05f4d312462543d7998f25f4eb9bb6"
 })
 
-module.exports = {
-		getTerm: function(callback){
+//module.exports = {
+		getTerm = function(callback){
 		uwclient.get('terms/list', {}, function(err, res){
 			callback(res["data"]["current_term"]);
 		});
+  }
+    validCourse = function(callback){
+    uwclient.get('/courses', {}, function(err, res){
+      callback(res);
+    });
 	}
-};
+//};
+
+validCourse(function(x){
+  for (var i = 0; i < x["data"].length; i++){
+      if ("CS" == x["data"][i]["subject"] && "135" == x["data"][i]["catalog_number"]){
+        console.log("hello");
+      }   
+  }
+});
