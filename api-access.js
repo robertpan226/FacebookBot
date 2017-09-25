79d80f14a7fcfa02c9facf5b71f41b04cd3af828
robertpan226 <robertpan226@gmail.com>
2017-09-24 23:33:42 -0400

    a

---
+++ api-access.js
@@ -0,0 +1,12 @@
+var uwaterlooApi = require('uwaterloo-api');
+var uwclient = new uwaterlooApi({
+	API_KEY : "2c05f4d312462543d7998f25f4eb9bb6"
+})
+
+module.exports = {
+		getTerm: function(callback){
+		uwclient.get('terms/list', {}, function(err, res){
+			callback(res["data"]["current_term"]);
+		});
+	}
+};
